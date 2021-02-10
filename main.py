90da7017c8d8acc76967c1b3e7ca818cf884e7e9
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2021-02-11 01:29:18 +0600

    testing insertion sort

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+from random import random
 from insertion_sort.basic_insertion_sort import insertion_sort
-
+from random import shuffle
 
 
 def main():
     # data to sort
-    need_to_sort = [8,4,2,3,5,6,1]
+    need_to_sort = []
+
+    for ranadom in range(1,10000):
+        need_to_sort.append(ranadom)
+    
+    shuffle(need_to_sort)
+
+    print(f"\n\nGiven Array \t==>\t {need_to_sort}")
 
     # run the algorithm
     insertion_sort(need_to_sort)
 
     # display sorted output
-    print(f"\n\nSorted Result \t==>\t {need_to_sort}")
+    print(f"\n\nSorted Array \t==>\t {need_to_sort}")
 
 if __name__=='__main__':
     main()
